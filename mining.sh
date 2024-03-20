ad771b0385756eb778f42356820827432276912d
wanzarians <111194523+wanzarians@users.noreply.github.com>
2024-03-20 21:41:55 +0700

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-o...​ && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u DCT7xs8yA5oWrVeohyNkcjhUrRWWh4b7Xd -p c=DGB -t 4
